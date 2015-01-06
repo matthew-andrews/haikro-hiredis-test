8f2810cf609ef0e2e01a32e2998a6409a1158add
Matthew Andrews <matt@mattandre.ws>
2015-01-06 09:52:47 +0000

    Add some files

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var hiredis = require("hiredis");
+var reader = new hiredis.Reader();
+
+
+app.get('/', function(req, res) {
+
+	// Data comes in
+	reader.feed("$5\r\nhello\r\n");
+
+	// Reply comes out
+	res.send(reader.get()); // => "hello"
+});
+
+
+app.listen(process.env.PORT || 5050);
